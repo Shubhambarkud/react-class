1c7ca7781cd7f55ba11a981ad17aeaddc51d05ea
Shubham Barkud <shubhambarkud2010@gmail.com>
2026-01-22 15:01:14 +0530

    third commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,13 @@
 import Chai from "./chai"
 
 function App() {
+  const username = "chai aur code"
 
   return (
 
     <>
       <Chai />
-      <h1>chai or react</h1>
+      <h1>chai or react {username}</h1>
     </>
   )
 }
